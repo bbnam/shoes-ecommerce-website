eb4eda7c3225b767d7debc9930fa174e18c7bbdc
bbnam <bbnam1591999@gmail.com>
2020-10-24 08:39:04 +0700

    login js

--- backend.py
+++ backend.py
@@ -17,16 +17,22 @@ app.config['MYSQL_DB'] = 'mydb'
 @app.route('/')
 def index():
     return render_template("index.html")
-@app.route('/login')
+@app.route('/login',methods = ['GET','POST'])
 def login():
+    # import pdb; pdb.set_trace()
     return render_template('login.html')
 
 @app.route('/check_login', methods = ['POST'])
 def check_login():
-   
     name = request.form['name']
     password = request.form['password']
+
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM user where user_name = %s", (name, ))
+    users = cur.fetchall()
+    cur.close()
     import pdb; pdb.set_trace()
-    return 'hi'
+
+    return jsonify(users)
 if __name__ == "__main__":
 	app.run(debug= True)
